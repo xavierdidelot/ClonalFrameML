dfa1ac8e3ed6fa70457a1d2f75623c65eb8e65ac
danny.wilson <danny.wilson@gmail.com>
2014-07-02 21:48:35 +0000

    Print more debug info

--- src/main.cpp
+++ src/main.cpp
@@ -4101,6 +4101,7 @@ double Baum_Welch(const marginal_tree &tree, const Matrix<Nucleotide> &node_nuc,
 			lenI += denTrans[1].todouble();
 			cout << "nmut = " << mutU_br << " nU = " << nsiU_br << " nsub = " << numEmiss[1][1].todouble() << " nI = " << denEmiss[1].todouble() << endl;
 			cout << "nU>I = " << numI_br << " dU = " << lenU_br << " nI>U = " << numTrans[1][0].todouble() << " dI = " << denTrans[1].todouble() << endl;
+			cout << "numTrans = " << numTrans[0][0].todouble() << " " << numTrans[0][1].todouble() << " " << numTrans[1][0].todouble() << " " << numTrans[0][0].todouble() << endl;
 		}
 	}
 	++neval;
@@ -4148,6 +4149,7 @@ double Baum_Welch(const marginal_tree &tree, const Matrix<Nucleotide> &node_nuc,
 				lenI += denTrans[1].todouble();
 				cout << "nmut = " << mutU_br << " nU = " << nsiU_br << " nsub = " << numEmiss[1][1].todouble() << " nI = " << denEmiss[1].todouble() << endl;
 				cout << "nU>I = " << numI_br << " dU = " << lenU_br << " nI>U = " << numTrans[1][0].todouble() << " dI = " << denTrans[1].todouble() << endl;
+				cout << "numTrans = " << numTrans[0][0].todouble() << " " << numTrans[0][1].todouble() << " " << numTrans[1][0].todouble() << " " << numTrans[0][0].todouble() << endl;
 			}
 		}
 		++neval;
