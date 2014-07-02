e3d68e3fc5c0c6bc85f4a806b6859bf2a5d12b52
danny.wilson@gmail.com <danny.wilson@gmail.com@8eae8a1d-a259-6cf4-e356-3999c86dedbe>
2014-07-02 22:35:10 +0000

    Print more debug info

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -4055,7 +4055,7 @@ mydouble mydouble_forward_backward_expectations_ClonalFrame_branch(const int dec
 						const int istrans = (int)(j!=k);
 						// Probability of transition from j to k given the data equals the joint likelihood of the data and transition from j to k, divided by marginal likelihood of the data
 						numTrans[j][k] += A[i][j]*ptrans[istrans]*pemis[k]*bnext[k]/ML;		// Note the use of bnext, not b
-						if(j==0 && k==1) cout << "pos = " << i << " numTrans[0][1] = " << (A[i][j]*ptrans[istrans]*pemis[k]*bnext[k]/ML).LOG() << endl;
+						if(j==0 && k==1) cout << "pos = " << i << " numTrans[0][1] = " << numTrans[j][k].todouble() << endl; //(A[i][j]*ptrans[istrans]*pemis[k]*bnext[k]/ML).LOG() << endl;
 					}
 					// Expected distance between sites equals actual distance weighted by the probability the 5prime site was in state j
 					denTrans[j] += dist*ppost[j];											// NB:- the denominator is the same for both destination states
